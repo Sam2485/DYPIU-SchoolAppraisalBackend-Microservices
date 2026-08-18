58d477b261e2cb78ecf62b25b151ffa18861ae43
Sam2485 <samarthgangji@gmail.com>
2026-08-18 12:15:39 +0530

    Add missing import org.springframework.data.jpa.repository.Modifying in RefreshTokenRepository

--- auth-user-service/src/main/java/com/director_appraisal/auth_user_service/repository/RefreshTokenRepository.java
+++ auth-user-service/src/main/java/com/director_appraisal/auth_user_service/repository/RefreshTokenRepository.java
@@ -3,8 +3,10 @@ package com.director_appraisal.auth_user_service.repository;
 import com.director_appraisal.auth_user_service.model.RefreshToken;
 import com.director_appraisal.auth_user_service.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
 import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
